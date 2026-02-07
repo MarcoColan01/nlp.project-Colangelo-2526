aac619bbe995482319d6990a97c98e15894ceb0e
MarcoColan01 <marcocola01@icloud.com>
2026-02-07 12:45:55 +0100

    Refactoring

---
+++ src/kaggle_download_IMDbSpoiler.py
@@ -0,0 +1,26 @@
+'''
+IMPORTANT: FOR AUTHENTICATION AND BE ABLE TO DOWNLOAD THE DATASET FROM KAGGLE, 
+THIS .py MODULE REQUIRES THAT A kaggle.json FILE, CONTAINING user AND api_key FIELDS, IS PROVIDED ON src FOLDER
+
+DATASET WILL BE SAVED ON data/raw DIRECTORY
+'''
+
+
+import os
+import json 
+import zipfile 
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+with open('kaggle.json', 'r') as f:
+    data = json.load(f)
+    os.environ['KAGGLE_USERNAME'] = data['username']
+    os.environ['KAGGLE_KEY'] = data['key']
+
+
+api = KaggleApi()
+api.authenticate()
+
+dataset = "rmisra/imdb-spoiler-dataset"
+print(f"Downloading {dataset}...")
+
+api.dataset_download_files(dataset, path='../data/raw', unzip=True)
